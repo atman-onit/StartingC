0f536026802c06df122771bd0935ae8d15b40193
atman-onit <atman.bharadwaj06@gmail.com>
2025-07-17 18:04:49 +0530

    coded

--- 1/operator.c
+++ 1/operator.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main() {
-     
+     int a ;
+     scanf("%d", &a);
+     int b;    
+     scanf("%d", &b);
+
+     printf ("Sum is %d\n", a + b);
+     printf ("Difference is %d\n", a - b);   
+     printf ("Product is %d\n", a * b);
+     printf ("Quotient is %d\n", a / b);
+     printf ("Remainder is %d\n", a % b);
+      
+     printf("Maths using operators");
 
 
     
