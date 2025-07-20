8aaf7a80c0c8d0e324187a36076f5aef3ee3eb2a
atman-onit <atman.bharadwaj06@gmail.com>
2025-07-20 22:53:09 +0530

    learning logic ops

--- 1/logic-ops.c
+++ 1/logic-ops.c
@@ -9,7 +9,8 @@ int main() {
 
     printf("enter number 2: (Either 1 or 0): \n");
     scanf("%d",&b);
-
+    
+        
     printf("The value of NOT(a) and NOT(b) is %d and %d respectively \n" , !a , !b);
     printf("The value of a AND b is: %d \n", a&&b );
     printf("The value of a OR b is %d \n", a || b);
