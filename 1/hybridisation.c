9389b6fcfc2d5e62778c2e82660dd95d07d89a45
atman-onit <atman.bharadwaj06@gmail.com>
2025-07-20 18:44:58 +0530

    Using if else

---
+++ 1/hybridisation.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    //This code is to identify the hybrisation of molecules according to their steric number
+
+    int steric;
+    printf("Enter steric number \n");
+    scanf("%d" , &steric);
+
+    if(steric<2){
+        printf("Molecule not hybridised");
+    }
+    
+    else if(steric==2){
+        printf("Hybridisation is sp");
+    }
+
+    else if(steric==3){
+        printf("Hybridisation is sp2");
+    }
+
+    else if(steric==4){
+        printf("Hybridisation is sp3");
+    }
+
+    else if(steric==5){
+        printf("Hybridisation is sp3d");
+    }
+
+    else if(steric==6){
+        printf("Hybridisation is sp3d2");
+    }
+
+    else if(steric>6){
+        printf("Hybridisation is complex");
+    }
+
+
+
+    
+    return 0;
+}
