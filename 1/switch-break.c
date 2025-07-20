5ae13a1c9d7096e0b013841a6f13047a6184f6f9
atman-onit <atman.bharadwaj06@gmail.com>
2025-07-20 22:27:08 +0530

    learning switch cases

---
+++ 1/switch-break.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    // this code is an example of switch break case in play
+
+    int cgpa ;
+    char grades ;
+
+    printf("enter cgpa: ");
+    scanf("%d" , &cgpa);
+
+    switch (cgpa){
+
+    case 10:
+        printf("You got the job as the CEO!");
+        break;
+    
+    case 9:
+        printf("You got the job as Boss!");
+        break;
+    
+    case 8: 
+        printf("You got the job as Senior manager!");
+        break;
+
+    case 7:
+        printf("You got the job as Junior manager");
+        break;
+    
+    case 6:
+        printf("All the best working as a call center boy");
+        break;
+    
+
+    default: printf("Its over gang");
+    }
+        
+    return 0;
+}
