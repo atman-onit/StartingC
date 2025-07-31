a4e0c1c4bba8f0d918ccc3254be562d69df6fd87
atman-onit <atman.bharadwaj06@gmail.com>
2025-07-31 23:42:28 +0530

    loops

--- 2/loopstart.c
+++ 2/loopstart.c
@@ -4,21 +4,14 @@ int main() {
     // code to print Hello world n times
 
     int n;
-    printf("Enter the number of time you want the message to get displayed \nBetween 1 to 20: ");
+    printf("Enter the number of time you want the message to get displayed: ");
     scanf("%d" , &n);
 
     int i = 1;
-
-    if (i <=20) {
-        
     while (i <= n) {
         printf("HELLO WORLD- Iteration number %d \n" , i);
         i++ ;
 
-    }
-}
-    else if (i >20) {
-        printf("The number has to be between 1 and 20");
     }
     return 0;
 }
