b191b93d43cefc8ca269b2bd6f691f5828ef3d70
atman-onit <atman.bharadwaj06@gmail.com>
2025-08-13 13:24:25 +0530

    Learning loops

---
+++ 2/quiz2.c
@@ -0,0 +1,11 @@
+//program to calculate sum of numbers occuring in multiplication table of 8 consider from 8x1  to 8x10
+#include <stdio.h>
+
+int main() {
+    int a, b=8, c;
+    for (a=1;a<=10;a++){
+        printf("%d \n" , c = a * b);
+        
+    } 
+    return 0;
+}
