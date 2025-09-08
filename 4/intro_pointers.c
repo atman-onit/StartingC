9805571ca476fbef8567ce01290b4e320fd394e0
atman-onit <atman.bharadwaj06@gmail.com>
2025-09-08 21:02:30 +0530

    learning pointers

--- 4/intro_pointers.c
+++ 4/intro_pointers.c
@@ -10,17 +10,14 @@ int main() {
    printf("Address of a is %p\n" , &a);
 
    //%p is used to print pointer in hexadecimal
-
+   
    int k = 99;
    int* k_p = &k;
    printf("address of k is %u\n" , k_p);
    printf("Address of k is %u\n" , &k);
     printf("Address of k is %p\n" , &k);
-    
-
-
-
    //%u is used to print pointer in decimal form.
+   
 
 
 return 0;
