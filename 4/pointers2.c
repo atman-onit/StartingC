2f184b92db0e5fa1e76fae2b16e424994e0aad9c
atman-onit <atman.bharadwaj06@gmail.com>
2025-09-20 19:19:31 +0530

    learning pointers

--- 4/pointers2.c
+++ 4/pointers2.c
@@ -3,7 +3,21 @@
 int main() {
     int a = 4;
     int* p_a = &a; //p_a is int pointer pointing to a/ has a's address
-    printf("value of a is %d" , a);
-    printf("address of a is %p in hexadecimal and %u in decimal", p_a, &a);
+    printf("value of a is %d\n" , a);
+    printf("address of a is %p in hexadecimal \nand %u in decimal\n", p_a, &a);
+
+    /*
+    in &a -- & is called "address of" operator
+    to print the value in the address a pointer is pointing to 
+    we use 
+    *(pointer) -- therefore * is called "value of" operator
+    or
+    *(&variable) = *(pointer pointing to a)
+    */
+    printf("Value of a is %d\n" , *p_a);
+    printf("value of a is %d\n" , *(&a));
+    //similarly
+    printf("address of a is %p\n" , &(*(&a)) );
     return 0;
+    
 }
