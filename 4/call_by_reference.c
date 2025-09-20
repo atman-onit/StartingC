4fcb6d987e20ad5c1256e0b6fe8157eeb5722051
atman-onit <atman.bharadwaj06@gmail.com>
2025-09-20 20:56:49 +0530

    learning pointers

---
+++ 4/call_by_reference.c
@@ -0,0 +1,16 @@
+//we can swap numbers by calling the address and changing it value.
+#include <stdio.h>
+
+int swap(int*a) //gets the address of variable
+{
+    return *a = 2; //returns value in address of variable as 2
+}
+int main() {
+    int a = 7;
+    int* p_a = &a;
+    printf("the value of a is %d" , swap(p_a));
+    
+
+    
+    return 0;
+}
