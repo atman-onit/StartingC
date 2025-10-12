26b3131a7a7d4a07d1f936f2825e0d3bd6604e01
atman-onit <atman.bharadwaj06@gmail.com>
2025-10-12 12:22:18 +0530

    learning structures

--- 7/array_of_arrays.c
+++ 7/array_of_arrays.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
-struct attendance{
+#include<string.h>
+struct student{
     char name[10];
     int attendance;
-
+    float weight;
 };
-int main() {
-    struct attendance a1,a2;
-    struct attendance name[10];
-    struct attendance attendance[10];
-    printf("Enter names\n");
-    for(int i=0; i<10;i++){
-        printf("Enter name %d: ", i+1);
-        scanf("%s", &name[i]);
-        
-    }
-    for(int i=0; i<10;i++){
-        printf("Enter attendance of student %d: ", i+1);
-        scanf("%s", &attendance[i]);
-        
-    }
-  
-        printf("%s %d", a1.name , a1.name);
+int main(){   
     
+    struct student s[3]; //array of 3 students
 
+    strcpy(s[0].name,"DUMBO");
+    s[0].attendance = 20;
+    s[0].weight = 40.3;
 
+    strcpy(s[1].name,"DUBO");
+    s[1].attendance = 30;
+    s[1].weight = 45.3;
 
+    strcpy(s[2].name,"DBO");
+    s[2].attendance = 20;
+    s[2].weight = 20.3;
+
+    for(int i=0;i<3;i++){
+        printf("Student %d: Name %s; Attendance %d; Weight %f\n", i+1, s[i].name,s[i].attendance,s[i].weight);
+
+    }
     return 0;
 }
